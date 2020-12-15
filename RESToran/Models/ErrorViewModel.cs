b6a6ddbb89a897d4a245f25c7404968af97e3d3f
elendur19 <mislavrobic@gmail.com>
2020-12-15 22:16:46 +0100

    initial project setup

---
+++ RESToran/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RESToran.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
