04248d41b57bd9077a59b2ee71a078a8532d2538
ivanivankovic <ivankovicivan11@gmail.com>
2021-01-15 02:58:19 +0100

    Init Desktop app and first few screens

---
+++ RESToran.PresentationLayer/Login.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace RESToran.PresentationLayer
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.registerControl1 = new RESToran.PresentationLayer.RegisterControl();
+            this.userControl11 = new RESToran.PresentationLayer.LoginControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcome to RESToran";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 515);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Register";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 462);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(428, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Would you like to log in or register?";
+            // 
+            // registerControl1
+            // 
+            this.registerControl1.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.registerControl1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.registerControl1.Location = new System.Drawing.Point(675, 0);
+            this.registerControl1.Margin = new System.Windows.Forms.Padding(5);
+            this.registerControl1.Name = "registerControl1";
+            this.registerControl1.Size = new System.Drawing.Size(563, 651);
+            this.registerControl1.TabIndex = 2;
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.userControl11.Location = new System.Drawing.Point(675, 0);
+            this.userControl11.Margin = new System.Windows.Forms.Padding(5);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(563, 651);
+            this.userControl11.TabIndex = 2;
+            this.userControl11.Load += new System.EventHandler(this.userControl11_Load);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1238, 651);
+            this.Controls.Add(this.registerControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userControl11);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Login";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private LoginControl userControl11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private RegisterControl registerControl1;
+    }
+}
---
+++ RESToran.PresentationLayer/LoginControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RESToran.PresentationLayer
+{
+    public partial class LoginControl : UserControl
+    {
+        public LoginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Username: " + textBox1.Text);
+            // TODO add atuh logic here
+            Login parent = (Login)this.Parent;
+            MainWindow newForm = new MainWindow();
+            parent.Hide();
+            newForm.Show();
+        }
+    }
+}
